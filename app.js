58a99713f7c1ab22d1e79e12d1de99ecbdb2c1e5
jeromeaking40 <jeromeaking40@yahoo.com>
2016-10-11 13:15:02 -0600

    Refactor SASS files and imported partials

--- app.js
+++ app.js
@@ -6,15 +6,23 @@ Router.$inject = ['$routeProvider'];
 function Router($routeProvider) {
 
     $routeProvider
-        .when('/', {
-            templateUrl: '/views/home.html',
-            controller: 'HomeCtrl as home'
+        .when("/", {
+            templateUrl: "/views/home.html",
+            controller: "HomeCtrl as home"
         })
-        .when('/about', {
-            templateUrl: '/views/about.html',
-            controller: 'AboutCtrl as about'
+        .when("/about", {
+            templateUrl: "/views/about.html",
+            controller: "AboutCtrl as about"
+        })
+        .when("/contact", {
+            templateUrl: "/views/contact.html",
+            controller: "ContactCtrl as contact"
+        })
+        .when("/sitemap", {
+            templateUrl: "/views/sitemap.html",
+            controller: "SitemapCtrl as sitemap"
         })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: "/"
         });
 }
