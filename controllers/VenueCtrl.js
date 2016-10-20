30eaedf3fd277380cd9a640f193a5501874539b3
jeromeaking40 <jeromeaking40@yahoo.com>
2016-10-20 16:29:11 -0600

    ADDED ERROR LOGS

--- controllers/VenueCtrl.js
+++ controllers/VenueCtrl.js
@@ -19,7 +19,7 @@ function VenueController($http, $routeParams) {
                 // lng=-105.358887
                 // start: 1476726501,
                 // end: 147673730,
-                key: 'cb4a71ff3c5e18429ad32c4c4bc8cdb3'
+                key: 'INSERT KEY'
             }
         });
     };
@@ -45,29 +45,6 @@ function VenueController($http, $routeParams) {
             console.error(err);
         });
 
-    // //FOURSQUARE API CALL
-    // vCtrl.callFourSquare = function() {
-    //     /* FourSquare API Call */
-    //     $http({
-    //             method: 'GET',
-    //             url: 'https://api.foursquare.com/v2/venues/search',
-    //             params: {
-    //                 client_id: 'XXEJ3UJTJBYJ3RETPR3G24EHCLNOIWVMVMMCRYEQMKBAMS1Q',
-    //                 client_secret: 'FCA0TMZ0WGESRGC0BWXQTIO3XZOVS1ZDMIH300TFVUVSZBZS',
-    //                 v: 20130815,
-    //                 near: "Denver",
-    //                 radius: 500,
-    //                 query: vCtrl.searchFourSquare,
-    //                 limit: 50
-    //             }
-    //         })
-    //         .then(function(response) {
-    //             console.log(response.data);
-    //             //ASSIGN A VARIABLE TO RETREIVE THE DATA
-    //             vCtrl.returnVenueRating = response.data;
-    //         }, function(response) {
-    //             console.log("Failure:", response);
-    //         });
-    // };
+
 
 }
